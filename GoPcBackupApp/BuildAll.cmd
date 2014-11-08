fc4133cd6cc1365a055dae5e1b44871b393d4328
Geo <GeoCode@Schiro.name>
2014-11-08 10:51:06 -0500

    Changed text reference from "Run Now" button to "GO" button.
    Signed-off-by: Geo <GeoCode@Schiro.name>

--- GoPcBackupApp/BuildAll.cmd
+++ GoPcBackupApp/BuildAll.cmd
@@ -4,7 +4,7 @@ set   AppName=GoPcBackup
 set    AppExe=%AppName%.exe
 set AppFolder=%AppName%App
 set DevServer=main
-set ExeFolder="\\%DevServer%\GoPcBackup\DotNet EXEs"
+set ExeFolder=\\%DevServer%\GoPcBackup\DotNet EXEs
 
 echo.
 echo *** Rebuild All %AppName% Versions ***
@@ -25,19 +25,19 @@ echo ***************************************************************************
 echo *** Note: Any new files added to the base project (eg. images, styles, etc.) ***
 echo ***       MUST also be added to "GoPcBackup.csproj (4.x)".                   ***
 echo ***                                                                          ***
-echo *** IF you FAIL to do this, the 4.x version will also fail to run!!!         ***
+echo *** IF you FAIL to do this, the 4.x version may compile but FAIL to run!!!   ***
 echo ********************************************************************************
 pause
 
 
 :: Remove local application folder, etc.
-if exist %AppFolder%\*.* rmdir /s/q %AppFolder%
-if exist %ExeFolder%\*.* rmdir /s/q %ExeFolder%
+if exist "%AppFolder%\*.*" rmdir /s/q "%AppFolder%"
+if exist "%ExeFolder%\*.*" rmdir /s/q "%ExeFolder%"
 if exist bin\*.* rmdir /s/q bin\
 if exist obj\*.* rmdir /s/q obj\
 
-xcopy /s \\%DevServer%\%AppName%\%AppFolder% %AppFolder%\
-cd %AppFolder%
+xcopy /s "\\%DevServer%\%AppName%\%AppFolder%" "%AppFolder%\"
+cd "%AppFolder%"
 
 
 :: Use version 3.5 make file by default.
@@ -45,12 +45,12 @@ echo.
 C:\Windows\Microsoft.NET\Framework\v3.5\msbuild       -p:Configuration=Release -verbosity:m
 
 echo.
-xcopy bin\Release\%AppExe% %ExeFolder%\"DotNet Version 3.5"\
+xcopy "bin\Release\%AppExe%" "%ExeFolder%\DotNet Version 3.5\"
 
 
 :: Switch to version 4 files.
-ren  %AppName%.csproj       "%AppName%.csproj (3.5)"
-ren "%AppName%.csproj (4.x)" %AppName%.csproj
+ren "%AppName%.csproj"       "%AppName%.csproj (3.5)"
+ren "%AppName%.csproj (4.x)" "%AppName%.csproj"
 
 ren "Setup Application Folder.exe"       "Setup Application Folder.exe (3.5)"
 ren "Setup Application Folder.exe (4.x)" "Setup Application Folder.exe"
@@ -59,15 +59,15 @@ echo.
 C:\Windows\Microsoft.NET\Framework\v4.0.30319\msbuild -p:Configuration=Release -verbosity:m
 :: (for 4.5 specifically)
 ::C:\Windows\Microsoft.NET\Framework\v4.0.30319\msbuild -p:Configuration=Release -verbosity:m -p:FrameworkPathOverride="C:\Program Files\Reference Assemblies\Microsoft\Framework\.NETFramework\v4.5"
-ren  %AppName%.csproj       "%AppName%.csproj (4.x)"
+ren "%AppName%.csproj"       "%AppName%.csproj (4.x)"
 ren "Setup Application Folder.exe"       "Setup Application Folder.exe (4.x)"
 
 echo.
-xcopy bin\Release\%AppExe% %ExeFolder%\"DotNet Version 4.x"\
+xcopy "bin\Release\%AppExe%" "%ExeFolder%\DotNet Version 4.x\"
 echo.
 pause
 cd ..
-if exist %AppFolder%\*.* rmdir /s/q %AppFolder%
+if exist "%AppFolder%\*.*" rmdir /s/q "%AppFolder%"
 goto :EOF
 
 
