6a9750dce440bb846dc9c70a60b3797dfa3b71ad
Geo <GeoCode@Schiro.name>
2018-06-23 21:14:06 -0400

    Updated "Setup Application Folder.exe" to run the app after moving it to the installation folder.

--- GoPcBackupApp/Properties/AssemblyInfo.cs
+++ GoPcBackupApp/Properties/AssemblyInfo.cs
@@ -50,6 +50,6 @@ using System.Windows;
 //
 // You can specify all the values or you can default the Build and Revision Numbers 
 // by using the '*' as shown below:
-[assembly: AssemblyVersion("4.34")]
-[assembly: AssemblyFileVersion("4.34")]
+[assembly: AssemblyVersion("4.35")]
+[assembly: AssemblyFileVersion("4.35")]
 [assembly: NeutralResourcesLanguageAttribute("en-US")]
